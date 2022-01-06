c412743f6009b9782344725bb3ca71268fd94922
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-06 20:41:51 -0300

    adicionei o segundo projeto python

---
+++ aula06.py
@@ -0,0 +1,15 @@
+#n1 = int(input('digite um valor'))
+#n2 = int(input('digite outro numero '))
+
+#r = n1 + n2
+
+#print('a soma entre',n1,'e', n2, 'é', r)
+
+#metodo 2
+
+n1 = int(input('digite um numero'))
+n2 = int(input('digite outro numero'))
+
+r = n1 + n2
+
+print('a soma entre {} e {} vale {}'.format(n1, n2,r))

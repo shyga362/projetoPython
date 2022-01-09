3e366f63f275d9aad9ac29b162f07625c715bd04
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-09 20:30:02 -0300

    arrumando erros

--- exercicio27.py
+++ exercicio27.py
@@ -1,5 +1,4 @@
-n = str(input("Digite o nome completo: ")).strip()
-nome = n.split()
-print("seu primeiro nome é {}".format(nome[0]))
-print("seu ultimo nome é {}".format(nome[len(nome)-1]))
-
+no = str(input("Digite o nome completo: ")).strip()
+nome = no.split()
+print('seu primeiro nome é {}'.format(nome[0]))
+print('seu ultimo nome é {}'.format(nome[len(nome)-1]))

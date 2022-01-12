bea92ca20f9bcdf2758d8abe58aae2a13d88b3c2
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-11 22:51:37 -0300

    começando o exercicio 33

---
+++ exercicio33.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+n3 = int(input("DIgite outro numero: "))
+
+if n1 > n2:
+    print("O primeiro numero é p maior")
+else:
+    print("")

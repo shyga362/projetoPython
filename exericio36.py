560846948863d86814125b334e1f699525eea81e
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-27 21:06:46 -0300

    começando as condições

--- exericio36.py
+++ exericio36.py
@@ -1,5 +1,12 @@
 casa = float(input("Digite o valor da casa: "))
 salario = float(input("Digite seu salario: "))
-anos = float(input("Digite o tempo em Anos que pretende pagar: "))
+anos = float(input("Digite o tempo em anos que pretende pagar: "))
+
+if anos > salario:
+    print("")
+elif:
+    print("")
+else:
+    print("")
 
 

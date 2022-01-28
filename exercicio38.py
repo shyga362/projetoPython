2556781e01904a3233349a94b970d7928c4793c7
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-28 18:49:52 -0300

    resolvendo exercicio 38

---
+++ exercicio38.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite um numero"))
+num2 = int(input("Digite outro numero"))
+
+if num1 > num2:
+    print("O primeiro valor é maior")
+elif num2 > num1:
+    print("O segundo valor é maior")
+else:
+    print("Não existe maior, os dois são iguais")

ba352c8f51f2eff675398c84c39c9c606c894e46
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-31 18:48:30 -0300

    criação do exercicio 39

---
+++ exercicio39.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade: "))
+
+if idade > 18:
+    print("Você deve se alistar!")
+elif idade < 18:
+    print("Você não precisa se alista!")
+elif idade == 18:
+    print("Você esta na hora de se alistar.")
+    

61e0cd0388b3dfa11b56a33c8fd4d45ba82f2e92
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-01 03:10:19 -0300

    terminando exercicio

--- exercicio39.py
+++ exercicio39.py
@@ -1,10 +1,17 @@
-idade = int(input("Digite o ano em que você nasceu: "))
-tempo = idade - 18 
+import datetime
+
+atual = datetime.date.today().year
+nasc = int(input("Qual o ano de nascimento? "))
+idade = atual - nasc
+
+print("Quem nasceu em {} tem {} anos em {}".format(nasc, idade, atual))
 
 
 if idade > 18:
+    tempo = 18 - idade
     print("Já passou {} anos de você se alistar!".format(tempo))
 elif idade < 18:
+    tempo = idade - 18
     print("Você não precisa se alista! falta {} anos".format(tempo))
 elif idade == 18:
     print("Você esta na hora de se alistar!")

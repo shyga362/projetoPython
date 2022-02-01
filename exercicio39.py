92477b98d2c048013eb017ad25d454a009dde21b
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-01-31 21:42:36 -0300

    teste 1

--- exercicio39.py
+++ exercicio39.py
@@ -1,9 +1,9 @@
 idade = int(input("Digite sua idade: "))
-
+tempo = idade - 18 
 if idade > 18:
-    print("Você deve se alistar!")
+    print("Já passou {} anos de você se alistar!".format(tempo))
 elif idade < 18:
-    print("Você não precisa se alista!")
+    print("Você não precisa se alista! falta {} anos".format(tempo))
 elif idade == 18:
-    print("Você esta na hora de se alistar.")
+    print("Você esta na hora de se alistar!")
     

b9e9764a4a5d25fbb3eac80f5caea0fef2e91ef4
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-01 20:53:10 -0300

    criando exercicio 40

---
+++ exercicio40.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite sua nota: "))
+nota2 = float(input("Digite outra nota: "))
+media = (nota1 + nota2)/2
+
+if media > 5.0:
+    print("REPROVADO")
+elif media == 5.0:
+    print("")

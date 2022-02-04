c843f6f756076785009d18a8b62176df1d2687cc
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-03 21:30:48 -0300

    resolução do exercicio 40

--- exercicio40.py
+++ exercicio40.py
@@ -1,8 +1,12 @@
-nota1 = float(input("Digite sua nota: "))
-nota2 = float(input("Digite outra nota: "))
-media = (nota1 + nota2)/2
+num1 = float(input("Digite sua primeira nota: "))
+num2 = float(input("Digite sua sugunda nora: "))
+media = (num1 + num2)/2
 
-if media > 5.0:
+if media < 5:
     print("REPROVADO")
-elif media == 5.0:
-    print("")
+elif media >= 5 < 6.9:
+    print("RECUPERAÇÃO")
+elif media > 7:
+    print("APROVADO")
+else:
+    print("ERRO")

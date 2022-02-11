ab82ab7a1160f4439cc74a18c82051f86a46fc0d
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-11 18:37:33 -0300

    teste

---
+++ exercicio42.py
@@ -0,0 +1,8 @@
+r1 = float(input("Digite um numero: "))
+r2 = float(input("Digite outro numero: "))
+r3 = float(input("Digite outro numero: "))
+
+"""if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print("Os seguimentos acima pode ser um triangulo")
+else: 
+    print("Os seguimentos acima n pode ser um triangulo")"""

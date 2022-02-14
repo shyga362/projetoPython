a3163a3832c425d22accc9774205560834d9ec0e
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-14 18:23:37 -0300

    resolvendo exercicio 42

--- exercicio42.py
+++ exercicio42.py
@@ -3,6 +3,12 @@ r2 = float(input("Digite outro numero: "))
 r3 = float(input("Digite outro numero: "))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print("Os seguimentos acima pode ser um triangulo")
+    print("Os seguimentos acima é um triangulo", end='')  
+    if r1 == r2 == r3:
+        print("equilatero")
+    elif r1 != r2 != r3 != r1:
+        print("escaleno")
+    else:
+        print("isoceles")
 else: 
-    print("Os seguimentos acima n pode ser um triangulo")
+    print("Os seguimentos acima n é um triangulo")

01a6021b64cd1572f10645fdb1657576723c3fa1
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-15 19:50:50 -0300

    .

--- exercicio43.py
+++ exercicio43.py
@@ -1,3 +1,11 @@
 peso = float(input("Digite seu peso: "))
 altura = float(input("Digite sua altura: "))
 
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print("Abaixo do peso")
+elif imc >= 18.5 < 25:
+    print("Peso Ideal")
+elif imc >= 25 <= 30:
+    print("Sobre Peso")

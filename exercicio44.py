c02b5f08bf202c2547abf113daae654b3ceafb53
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-21 21:51:38 -0300

    terminado exercicio 44

--- exercicio44.py
+++ exercicio44.py
@@ -3,24 +3,25 @@ preco = float(input("Digite o preço do produto: "))
 print("""Digite o metoto de pagamento: 
 1 - dinherio/cheque 
 2 - a vista no cartão 
-3 - parcelado 
+3 - parcelado 2x no cartão
+4 - parecelado 3x ou mais no cartão
 """)
 opcao = int(input("Digite sua opção: "))
 
 
-if opcao == 3:
-    opcao = int(input("Enquantas vezes quer parcelar? "))
-    if opcao <= 2:
-        print("O valor fico de {}".format(preco/2))
-    elif opcao >=3:
-        juros = preco + (preco * 20) /100
-        
-        print("O juros é de 20%. O valor ficou {}".format(juros))
-elif opcao == 1:
+
+if opcao == 1:
     desconto = preco - (preco * 10) / 100
     print("Seu desconto foi de 10%. E o valor agora é {}".format(desconto))
 elif opcao == 2:
     desconto = preco - (preco * 5) /100
     print("Seu desconto foi de 5%. E o valor agora é {} ".format(desconto))
+elif opcao == 3:
+        print("O valor fico de {}".format(preco/2))
+elif opcao == 4:
+    juros = preco + (preco * 20 / 100)
+    quantidade = int(input("Quantas parcelas? "))
+    parcelas = juros / quantidade
+    print("O valor ficou de {} de {}x o total com juros ficou de {}".format(parcelas, quantidade, juros))
 else:
     print("ERRO")

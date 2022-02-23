95df22900e152a03370289d2b59105c059f56650
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-23 16:39:55 -0300

    terminando parte do jogo

---
+++ exercicio45.py
@@ -0,0 +1,14 @@
+from random import randint
+itemn = ('papel', 'pedra', 'tesolra')
+computador = randint(0, 2)
+#print("O computador escolheu {}".format(itens[computador]))
+print("""Suas opções
+0 - Papel
+1 - pedra
+2 - tesolra
+""")
+jogador = int(input("Qual sua jogada? "))
+print("Computador jogou {}".format(itens[computador]))
+print("Jogador jogou {}".format(itens[jogador]))
+
+

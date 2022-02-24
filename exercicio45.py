d6e2c81c96318d2af7d1eff5a627fc939eb981af
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-02-24 17:45:30 -0300

    terminando exercicio

--- exercicio45.py
+++ exercicio45.py
@@ -11,4 +11,30 @@ jogador = int(input("Qual sua jogada? "))
 print("Computador jogou {}".format(itens[computador]))
 print("Jogador jogou {}".format(itens[jogador]))
 
-
+if computador == 0:
+    if jogador == 0:
+        print("Empate")
+    elif jogador == 1:
+        print("Computador venceu")
+    elif jogador == 2:
+        print("Jogador venceu")
+    else:
+        print("ERRO")
+elif computador == 1:
+    if jogador == 0:
+        print("Jogador venceu")
+    elif jogador == 1:
+        print("Empate")
+    elif jogador == 2:
+        print("Computador venceu")
+    else:
+        print("ERRO")
+elif computador == 2:
+    if jogador == 0:
+        print("Computador venceu")
+    elif jogador == 1:
+        print("Jogador venceu")
+    elif jogador == 2:
+        print("Empate")
+    else:
+        print("ERRO")

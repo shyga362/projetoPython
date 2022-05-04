bbb635fd4c3e5c2bea04f63c7f3d71bc25fcfb34
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-05-04 16:01:45 -0300

    consertando exercicio

--- exercicio9.py
+++ exercicio9.py
@@ -1,15 +1,12 @@
-n0 = int(input('digite um numero: '))
+num = int(input('digite um numero: '))
 
-n1 = n0*1
-n2 = n0*2
-n3 = n0*3
-n4 = n0*4
-n5 = n0*5
-n6 = n0*6
-n7 = n0*7
-n8 = n0*8
-n9 = n0*9
-n10 = n0*10
-
-print('{} x 1 = {} \n {} x 2 = {}\n {} x 3 = {}\n {} x 4 = {}\n {} x 5 = {}'.format(n1, n1, n2, n2, n3,n3, n4, n4, n5, n5,))
-print('{} x 6 = {}\n {} x 7 = {}\n {} x 8 = {}\n {} x 9 = {}\n {} x 10 ={}'.format(n6, n6, n7, n7, n8, n8, n9, n9, n10, n10,))
+print("{} x {:2} = {}".format(num, 1, num*1))
+print("{} x {:2} = {}".format(num, 2, num*2))
+print("{} x {:2} = {}".format(num, 3, num*3))
+print("{} x {:2} = {}".format(num, 4, num*4))
+print("{} x {:2} = {}".format(num, 5, num*5))
+print("{} x {:2} = {}".format(num, 6, num*6))
+print("{} x {:2} = {}".format(num, 7, num*7))
+print("{} x {:2} = {}".format(num, 8, num*8))
+print("{} x {:2} = {}".format(num, 9, num*9))
+print("{} x {:2} = {}".format(num, 10, num*10))

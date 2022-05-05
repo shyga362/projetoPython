7f5bfbefa542eed44453e3eeb1a43a9ba3840944
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-05-05 16:15:37 -0300

    resolução do exercicio 52

--- exercicio52.py
+++ exercicio52.py
@@ -1,3 +1,14 @@
 num = int(input("Digite um numero: "))
+total = 0
 for c in range(1, num+1):
-    print(c)
+    print(c, end=' ')
+    if num % 2 == 0:
+        print('\033[33m', end='')
+    else:
+        print('\033[31m', end='')
+    print("{}".format(c), end='')
+print('\n\033[mO número {} foi divisivel {} vezes'.format(num, total))
+if total == 2:
+    print("por isso ele é primo")
+else:
+    print("por isso ele não é primo")

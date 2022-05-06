e52b5d552a2b5b02731ecd85c9661815f0ae9013
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-05-06 14:59:15 -0300

    terminando exercicio 53

---
+++ exercicio53.py
@@ -0,0 +1,11 @@
+frase = str(input("Digite um frase: ")).strip().upper()
+palavra = frase.split()
+junto = ''.join(palavra)
+inverso = ''
+for letra in range(len(junto) - 1, - 1, - 1):
+    inverso += junto[letra]
+print(junto, inverso)
+if inverso == junto:
+    print("Temos um palindromo")
+else:
+    print("A frase n é um palindromo")
